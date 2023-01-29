333355b44d79c4b584fee5d5ff7b07ee11ea6624
ChrisOtt0 <sophusott@gmail.com>
2023-01-29 20:48:48 +0100

    full test

--- src/alphcon.rs
+++ src/alphcon.rs
@@ -68,4 +68,236 @@ impl AlphCon {
             self.dot();
         }
     }
+
+    pub fn d(&mut self) {
+        self.dash();
+        for _ in 0..2 {
+            self.dot();
+        }
+    }
+
+    pub fn e(&mut self) {
+        self.dot();
+    }
+
+    pub fn f(&mut self) {
+        for _ in 0..2 {
+            self.dot();
+        }
+        self.dash();
+        self.dot();
+    }
+
+    pub fn g(&mut self) {
+        for _ in 0..2 {
+            self.dash();
+        }
+        self.dot();
+    }
+
+    pub fn h(&mut self) {
+        for _ in 0..4 {
+            self.dot();
+        }
+    }
+
+    pub fn i(&mut self) {
+        for _ in 0..2 {
+            self.dot();
+        }
+    }
+
+    pub fn j(&mut self) {
+        self.dot();
+        for _ in 0..3 {
+            self.dash();
+        }
+    }
+
+    pub fn k(&mut self) {
+        self.dash();
+        self.dot();
+        self.dash();
+    }
+
+    pub fn l(&mut self) {
+        self.dot();
+        self.dash();
+        for _ in 0..2 {
+            self.dot();
+        }
+    }
+
+    pub fn m(&mut self) {
+        for _ in 0..2 {
+            self.dash();
+        }
+    }
+
+    pub fn n(&mut self) {
+        self.dash();
+        self.dot();
+    }
+
+    pub fn o(&mut self) {
+        for _ in 0..3 {
+            self.dash();
+        }
+    }
+
+    pub fn p(&mut self) {
+        self.dot();
+        for _ in 0..2 {
+            self.dash();
+        }
+        self.dot();
+    }
+    
+    pub fn q(&mut self) {
+        for _ in 0..2 {
+            self.dash();
+        }
+        self.dot();
+        self.dash();
+    }
+
+    pub fn r(&mut self) {
+        self.dot();
+        self.dash();
+        self.dot();
+    }
+    
+    pub fn s(&mut self) {
+        for _ in 0..3 {
+            self.dot();
+        }
+    }
+
+    pub fn t(&mut self) {
+        self.dash();
+    }
+
+    pub fn u(&mut self) {
+        for _ in 0..2 {
+            self.dot();
+        }
+        self.dash();
+    }
+
+    pub fn v(&mut self) {
+        for _ in 0..3 {
+            self.dot();
+        }
+        self.dash();
+    }
+
+    pub fn w(&mut self) {
+        self.dot();
+        for _ in 0..2 {
+            self.dash();
+        }
+    }
+
+    pub fn x(&mut self) {
+        self.dash();
+        for _ in 0..2 {
+            self.dot();
+        }
+        self.dash();
+    }
+
+    pub fn y(&mut self) {
+        self.dash();
+        self.dot();
+        for _ in 0..2 {
+            self.dash();
+        }
+    }
+
+    pub fn z(&mut self) {
+        for _ in 0..2 {
+            self.dash();
+        }
+        for _ in 0..2 {
+            self.dot();
+        }
+    }
+
+    pub fn one(&mut self) {
+        self.dot();
+        for _ in 0..4 {
+            self.dash();
+        }
+    }
+
+    pub fn two(&mut self) {
+        for _ in 0..2 {
+            self.dot();
+        }
+        for _ in 0..3 {
+            self.dash();
+        }
+    }
+
+    pub fn three(&mut self) {
+        for _ in 0..3 {
+            self.dot();
+        }
+        for _ in 0..2 {
+            self.dash();
+        }
+    }
+
+    pub fn four(&mut self) {
+        for _ in 0..4 {
+            self.dot();
+        }
+        self.dash();
+    }
+
+    pub fn five(&mut self) {
+        for _ in 0..5 {
+            self.dot();
+        }
+    }
+
+    pub fn six(&mut self) {
+        self.dash();
+        for _ in 0..4 {
+            self.dot();
+        }
+    }
+
+    pub fn seven(&mut self) {
+        for _ in 0..2 {
+            self.dash();
+        }
+        for _ in 0..3 {
+            self.dot();
+        }
+    }
+
+    pub fn eight(&mut self) {
+        for _ in 0..3 {
+            self.dash();
+        }
+        for _ in 0..2 {
+            self.dot();
+        }
+    }
+
+    pub fn nine(&mut self) {
+        for _ in 0..4 {
+            self.dash();
+        }
+        self.dot();
+    }
+
+    pub fn zero(&mut self) {
+        for _ in 0..5 {
+            self.dash();
+        }
+    }
+
 }
+
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 mod alphcon;
+use std::io::{self, Write};
+
 use alphcon::AlphCon;
 
 fn main() {
@@ -6,11 +8,128 @@ fn main() {
     let unit = 250;
     let mut ac = AlphCon::new(port, unit);
 
-    ac.word_space();
-    ac.a();
-    ac.letter_space();
-    ac.b();
-    ac.letter_space();
-    ac.c();
-    ac.letter_space();
+    loop {
+        let mut input = String::new();
+
+        println!("Input text: "); 
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut input).unwrap();
+        let input = input
+            .trim()
+            .to_lowercase();
+        
+        ac.word_space();
+        for c in input.chars() {
+            if c == 'a' {
+                ac.a();
+            } else if c == 'b' {
+                ac.b();
+            } else if c == 'c' {
+                ac.c();
+            } else if c == 'd' {
+                ac.d();
+            } else if c == 'e' {
+                ac.e();
+            } else if c == 'f' {
+                ac.f();
+            } else if c == 'g' {
+                ac.g();
+            } else if c == 'h' {
+                ac.h();
+            } else if c == 'i' {
+                ac.i();
+            } else if c == 'j' {
+                ac.j();
+            } else if c == 'k' {
+                ac.k();
+            } else if c == 'l' {
+                ac.l();
+            } else if c == 'm' {
+                ac.m();
+            } else if c == 'n' {
+                ac.n();
+            } else if c == 'o' {
+                ac.o();
+            } else if c == 'p' {
+                ac.p();
+            } else if c == 'q' {
+                ac.q();
+            } else if c == 'r' {
+                ac.r();
+            } else if c == 's' {
+                ac.s();
+            } else if c == 't' {
+                ac.t();
+            } else if c == 'u' {
+                ac.u();
+            } else if c == 'v' {
+                ac.v();
+            } else if c == 'w' {
+                ac.w();
+            } else if c == 'x' {
+                ac.x();
+            } else if c == 'y' {
+                ac.y();
+            } else if c == 'z' {
+                ac.z();
+            } else if c == '1' {
+                ac.one();
+            } else if c == '2' {
+                ac.two();
+            } else if c == '3' {
+                ac.three();
+            } else if c == '4' {
+                ac.four();
+            } else if c == '5' {
+                ac.five();
+            } else if c == '6' {
+                ac.six();
+            } else if c == '7' {
+                ac.seven();
+            } else if c == '8' {
+                ac.eight();
+            } else if c == '9' {
+                ac.nine();
+            } else if c == '0' {
+                ac.zero();
+            } else if c == ' ' {
+                ac.word_space();
+            } 
+             ac.letter_space();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
 }
